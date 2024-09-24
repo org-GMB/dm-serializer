e8a2aa71575e250f953f9c35519e15400132a79f
osangu <osangu.dev@gmail.com>
2024-09-24 13:14:31 +0900

    initial commit

---
+++ src/test/kotlin/xyg/orggmb/dmserializer/DmSerializerApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyg.orggmb.dmserializer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DmSerializerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
