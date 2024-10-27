dfa6779901ea7205dddee0337bcb874de111fca8
osangu <osangu.dev@gmail.com>
2024-10-27 20:27:00 +0900

    convert to kotlin

---
+++ src/main/kotlin/xyg/orggmb/dmserializer/DmSerializerApplication.kt
@@ -0,0 +1,11 @@
+package xyg.orggmb.dmserializer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DmSerializerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DmSerializerApplication>(*args)
+}
